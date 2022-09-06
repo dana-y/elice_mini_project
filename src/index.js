e580ef00c4ce050f3905fb00e2654e9629ec4430
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-09-07 04:15:29 +0900

    set: recoil 초기세팅

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import { mainTheme } from './theme';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
@@ -12,9 +13,11 @@ const root = createRoot(container);
 root.render(
   <>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={mainTheme}>
-        <App tab='home' />
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={mainTheme}>
+          <App tab='home' />
+        </ThemeProvider>
+      </RecoilRoot>
     </QueryClientProvider>
   </>,
 );
